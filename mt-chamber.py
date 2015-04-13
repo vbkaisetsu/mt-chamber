fed11602df17d9faaf8a66276b7fa699b2b3ed26
Koichi Akabe <vbkaisetsu@gmail.com>
2015-04-13 21:09:45 +0900

    Add an optional argument 'FILE' to specify a script in arguments

--- mt-chamber.py
+++ mt-chamber.py
@@ -7,19 +7,34 @@ from argparse import ArgumentParser
 
 def main():
 	parser = ArgumentParser()
-	parser.add_argument("--threads", type=int, default=1, help="Number of threads")
-	parser.add_argument("--buffersize", type=int, default=100, help="Size of buffer")
+	parser.add_argument("-t", "--threads", type=int, default=1, help="number of threads (default: 1)")
+	parser.add_argument("-b", "--buffersize", type=int, default=100, help="size of buffer (default: 100)")
+	parser.add_argument("FILE", nargs="?", default=None, help="Chamber script file (default: stdin)")
 	args = parser.parse_args()
 
 	if args.threads < 1:
 		print("--threads must be larger than 1", file=sys.stderr)
 		return
+
 	if args.buffersize <= args.threads:
 		print("--buffersize must be larger than --threads", file=sys.stderr)
 		return
 
-	script = ScriptRunner(sys.stdin, threads=args.threads, buffersize=args.buffersize)
+	if args.FILE is None:
+		fstream = sys.stdin
+	else:
+		try:
+			fstream = open(args.FILE, "r")
+		except IOError:
+			print("Error: could not open `%s'" % args.FILE, file=sys.stderr)
+			return
+
+	script = ScriptRunner(fstream, threads=args.threads, buffersize=args.buffersize)
 	script.run()
+
+	if fstream:
+		fstream.close()
+
 
 if __name__ == "__main__":
 	main()
