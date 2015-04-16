1671ee6f9655423629a6b399d36c535be1b35749
Koichi Akabe <vbkaisetsu@gmail.com>
2015-04-16 09:41:30 +0900

    Add ShutdownSocketConnection plugin command

---
+++ plugins/ShutdownSocketConnection.py
@@ -0,0 +1,25 @@
+import socket
+
+
+class Command:
+
+	InputSize = 1
+	OutputSize = 0
+	MultiThreadable = True
+	ShareResources = True
+
+	def __init__(self, how):
+		if "r" in how and "w" in how:
+			self.how = socket.SHUT_RDWR
+		if "r" in how:
+			self.how = socket.SHUT_RD
+		if "w" in how:
+			self.how = socket.SHUT_WR
+		else:
+			raise Exception("Option \"how\" should have \"r\", \"w\" or both characters")
+
+
+	def routine(self, instream):
+		conn = instream[0]
+		conn.shutdown(self.how)
+		return ()
